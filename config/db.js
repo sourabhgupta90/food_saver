ab084abe7e0b8b43677c97fc1a60aecf83838403
Deepak Bhoria <dpk22dev@gmail.com>
2014-11-01 15:16:05 +0530

    old files deleted

--- config/db.js
+++
@@ -1,6 +0,0 @@
-// config/db.js
-//format: url : 'mongodb://<user>:<pass>@mongo.onmodulus.net:27017/uw45mypu'
-    module.exports = {
-        url : 'mongodb://localhost:27017/nerds'
-    }
-
--- public/javascripts/controllers/NerdCtrl.js
+++
@@ -1,6 +0,0 @@
-// public/js/controllers/NerdCtrl.js
-angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
-
-    $scope.tagline = 'Nerd Controller!';
-
-});
