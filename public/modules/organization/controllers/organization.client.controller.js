6d06fbe95b4d23c26bded2428596ce03fd9f1e04
sourabh.gupta <sourabh.gupta@ibibogroup.com>
2014-12-03 11:21:45 +0530

    conflict resolve

--- public/modules/organization/controllers/organization.client.controller.js
+++ public/modules/organization/controllers/organization.client.controller.js
@@ -3,10 +3,10 @@
 angular.module('organization').controller('OrganizationController', ['$scope', '$stateParams', '$location', '$http', 'Authentication', 'Organization', 'createOrganization',
     function($scope, $stateParams, $location, $http, Authentication, Organization, createOrganization) {
         
-        $scope.user = user;
-        this.minLifeSpan = user.minLifeSpan;
         $scope.authentication = Authentication;
-        
+        $scope.user = Authentication.user;
+        this.minLifeSpan = Authentication.user.minLifeSpan;
+
         $scope.create = function() {
             $http.post('/organization/mqf/1/', {
                 'mls': this.user.minLifeSpan,
