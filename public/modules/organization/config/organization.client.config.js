6e68fa27f6963656f4b37967f9311ec7acd3f622
sourabh.gupta <sourabh.gupta@ibibogroup.com>
2014-12-04 07:17:07 +0530

    remove unused files

--- public/modules/organization/config/organization.client.config.js
+++ public/modules/organization/config/organization.client.config.js
@@ -6,6 +6,5 @@ angular.module('organization').run(['Menus',
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Organization', 'organization', 'dropdown', '/organization(/create)?');
 		Menus.addSubMenuItem('topbar', 'organization', 'List Organization', 'organization');
-		Menus.addSubMenuItem('topbar', 'organization', 'New Organization', 'organization/create');
 	}
 ]);
--- public/modules/organization/config/organization.client.routes.js
+++ public/modules/organization/config/organization.client.routes.js
@@ -8,18 +8,6 @@ angular.module('organization').config(['$stateProvider',
 		state('listOrganization', {
 			url: '/organization',
 			templateUrl: 'modules/organization/views/list-articles.client.view.html'
-		}).
-		state('createOrganization', {
-			url: '/organization/create',
-			templateUrl: 'modules/organization/views/create-article.client.view.html'
-		}).
-		state('viewOrganization', {
-			url: '/organization/:organizationId',
-			templateUrl: 'modules/organization/views/view-article.client.view.html'
-		}).
-		state('editOrganization', {
-			url: '/organization/:organizationId/edit',
-			templateUrl: 'modules/organization/views/edit-article.client.view.html'
 		});
 	}
 ]);
